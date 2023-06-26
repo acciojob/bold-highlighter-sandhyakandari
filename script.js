5ed3d5d30df8822df9450b8ebfb0add6efebc4ad
sandhya <114242237+sandhyakandari@users.noreply.github.com>
2023-06-26 12:27:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
     //Write your code here
-
-
+ let stro=document.getElementsByTagName("strong");
+  let len=stro.length;
+  for(let i=0;i<len;i++){
+   stro[i].style.color="green";
+  }
 }
 
 
 function return_normal() {
-    //Write your code here
-
-    
+ let stro=document.getElementsByTagName("strong");
+  let len=stro.length;
+  for(let i=0;i<len;i++){
+   stro[i].style.color="black";
+  }
 }
